2d872f09d54e62f01d0eea79a67996a805558316
Eli_Tofu <elijaljmsalvan@gmail.com>
2025-01-13 15:03:25 +0800

    register implemented

--- user_authentication/views.py
+++ user_authentication/views.py
@@ -1,5 +1,6 @@
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login ,logout
 from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
 from django.contrib import messages
 
 def user_login(request):
@@ -10,9 +11,42 @@ def user_login(request):
         user = authenticate(username=username, password=passw)
 
         if user is None:
+            # If authentication failed
             messages.error(request, 'Invalid Login')
             return redirect('accounts:login')
         else:
+            #If authentication is successful
             login(request, user)
             return redirect('home')
     return render(request, 'user_authentication/login.html')
+
+def user_register(request):
+    if request.method == "POST":
+        first_name = request.POST.get("first_name")
+        last_name = request.POST.get("last_name")
+        username = request.POST.get("username")
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+
+        # check if user already exist
+        user = User.objects.filter(username=username)
+
+        if user.exists():
+            messages.info(request, 'Username already exist!')
+            return redirect('authentication:register')
+        
+        # create new user object with provided info
+        user = User.objects.create_user(first_name=first_name,
+                                        last_name=last_name,
+                                        username=username,
+                                        email=email,
+                                        password=password)
+        
+        # save user password 
+        user.set_password(password)
+        user.save()
+
+        messages.info(request, 'Account created successfully!')
+        return redirect('home')
+    
+    return render(request, 'user_authentication/register.html')
--- walk_well/urls.py
+++ walk_well/urls.py
@@ -5,5 +5,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('dashboard.urls')),
-    path("accounts/", include("user_authentication.urls")),
+    path("accounts/", include(("user_authentication.urls", "accounts"), namespace="accounts")),
 ]
